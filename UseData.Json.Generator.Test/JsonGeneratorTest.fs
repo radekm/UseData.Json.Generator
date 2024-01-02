61d1a9ef76e69742edfefed96c34c85c82f50b0e
Radek Micek <radek.micek@gmail.com>
2024-01-02 14:37:29 +0100

    Generate parsers for records with both required and optional fields

--- UseData.Json.Generator.Test/JsonGeneratorTest.fs
+++ UseData.Json.Generator.Test/JsonGeneratorTest.fs
@@ -8,8 +8,23 @@ open NUnit.Framework
 let ``simple record`` () =
     let input = Ast.Record ("Person", [ { Name = "Name"; Type = Ast.JIdent ["string"] }
                                         { Name = "Age"; Type = Ast.JIdent ["int"] } ])
-    let expected = [| "// TODO: Generate deserializer for record with fields Name, Age."
-                      "static member ParseJson(v : UseData.Json.JsonValue) : Person = failwith \"TODO\""
+    let expected = [| "static member ParseJson(v : UseData.Json.JsonValue) : Person ="
+                      "    { Name = v |> UJson.field \"Name\" UJson.string"
+                      "      Age = v |> UJson.field \"Age\" UJson.int"
+                      "    }"
                    |]
+    let actual = JsonGenerator.run input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``record with optional fields`` () =
+    let input = Ast.Record ("Foo", [ { Name = "Bar"; Type = Ast.JApp (Ast.JIdent ["voption"], [Ast.JIdent ["bool"]]) }
+                                     { Name = "Baz"; Type = Ast.JApp (Ast.JIdent ["option"], [Ast.JIdent ["int"]]) } ])
+    let expected =
+        [| "static member ParseJson(v : UseData.Json.JsonValue) : Foo ="
+           "    { Bar = v |> UJson.fieldOpt \"Bar\" UJson.bool"
+           "      Baz = v |> UJson.fieldOpt \"Baz\" UJson.int |> function ValueNone -> None | ValueSome x -> Some x"
+           "    }"
+        |]
     let actual = JsonGenerator.run input
     Assert.That(actual, Is.EqualTo(expected))
--- UseData.Json.Generator/JsonGenerator.fs
+++ UseData.Json.Generator/JsonGenerator.fs
@@ -1,8 +1,44 @@
 module JsonGenerator
 
+let parseSingleValue (longIdent : string list) =
+    match longIdent with
+    | ["string"] -> "UJson.string"
+    | ["DateTimeOffset"] -> "UJson.dateTimeOffset"
+    | ["int"] -> "UJson.int"
+    | ["uint"] -> "UJson.uint"
+    | ["int64"] -> "UJson.int64"
+    | ["uint64"] -> "UJson.uint64"
+    | ["decimal"] -> "UJson.decimal"
+    | ["bool"] -> "UJson.bool"
+    | x -> x |> String.concat "." |> fun tpe -> tpe + ".ParseJson"
+
+let parseRecordField (field : Ast.RecordField) : string =
+    match field.Type with
+    | Ast.JIdent longIdent -> $"v |> UJson.field \"%s{field.Name}\" %s{parseSingleValue longIdent}"
+    | Ast.JApp (Ast.JIdent ["voption"], [Ast.JIdent longIdent]) ->
+        $"v |> UJson.fieldOpt \"%s{field.Name}\" %s{parseSingleValue longIdent}"
+    | Ast.JApp (Ast.JIdent ["option"], [Ast.JIdent longIdent]) ->
+        let convert = "function ValueNone -> None | ValueSome x -> Some x"
+        $"v |> UJson.fieldOpt \"%s{field.Name}\" %s{parseSingleValue longIdent} |> %s{convert}"
+    | _ -> failwithf "Field %s has unsupported type: %A" field.Name field.Type
+
+let generateRecordParser (fields : Ast.RecordField list) = seq {
+    if fields.IsEmpty then
+        failwith "Record has no fields"
+    let mutable firstField = true
+    for field in fields do
+        let parse = parseRecordField field
+        if firstField then
+            yield $"    {{ %s{field.Name} = %s{parse}"
+            firstField <- false
+        else
+            yield $"      %s{field.Name} = %s{parse}"
+    yield "    }"
+}
+
 let run (simpleType : Ast.SimpleType) =
     match simpleType with
     | Ast.Record (name, fields) ->
-        let fieldNames = fields |> Seq.map _.Name |> String.concat ", "
-        [| $"// TODO: Generate deserializer for record with fields %s{fieldNames}."
-           $"static member ParseJson(v : UseData.Json.JsonValue) : %s{name} = failwith \"TODO\"" |]
+        Array.append
+            [| $"static member ParseJson(v : UseData.Json.JsonValue) : %s{name} =" |]
+            (generateRecordParser fields |> Array.ofSeq)
